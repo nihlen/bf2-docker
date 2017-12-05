18656f93999206f0d0924a303f89a6cc0ed0a3a2
nihlen <alex.nihlen@gmail.com>
2017-12-05 22:50:18 +0100

    Overwrite dest only if empty. Added new serversettings file

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ Building
 ---
 
     docker image build -t bf2 .
-    docker run --name bf2server --hostname bf2server --network bridge --interactive --volume <host directory>:/home/bf2/srv --privileged=true -p 4711:4711/tcp -p 4712:4712/tcp -p 16567:16567/udp -p 27901:27901/udp -p 29900:29900/udp bf2:latest
+    docker run --name bf2server --interactive --volume <host directory>:/home/bf2/srv -p 4711:4711/tcp -p 4712:4712/tcp -p 16567:16567/udp -p 27901:27901/udp -p 29900:29900/udp bf2:latest
--- assets/start.sh
+++ assets/start.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-# Move from temp to persisted folder (symlink instead?)
-mv -n /home/bf2/tmp/srv/* /home/bf2/srv/
-rm -rf /home/bf2/tmp/
+TMP="/home/bf2/tmp"
+VOLUME="/home/bf2"
 
-# Replace serversettings.con
-# TODO
+SRC="$TMP/srv/"
+DEST="$VOLUME/srv/"
+
+# Move from temp to persisted folder (symlink instead?)
+if [ "$(ls -A $DEST)" ]; then
+    echo "$DEST is not empty"
+else
+    echo "$DEST is empty. Moving server files..."
+
+    # Move server files (-n without overwriting)
+    mv -n $SRC* $DEST
+
+    # Overwrite settings file
+    mv "$TMP/serversettings.con" "$DEST/mods/bf2/settings/"
+
+    # Delete our tmp directory
+    rm -rf $SRC
+fi
 
 # Start Battlefield 2 server
 export TERM=xterm
-cd /home/bf2/srv
+cd $DEST
 ./start.sh
